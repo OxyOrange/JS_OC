0fc6b642ae886fda2cbde2ec8c5212febbc0c3e0
104973qi <zhanghouqi_ios@163.com>
2018-06-26 12:03:41 +0800

    m

---
+++ WKWebViewMessageHandlerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewMessageHandlerDemo
+//
+//  Created by Orange on 16/9/12.
+//  Copyright © 2016年 Orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKWebViewMessageHandlerDemo/WKWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKWebViewController.h
+//  WKWebViewMessageHandlerDemo
+//
+//  Created by Orange on 16/9/12.
+//  Copyright © 2016年 Orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKWebViewController : UIViewController
+
+@end
